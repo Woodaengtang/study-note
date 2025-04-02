fab90f7a75b56ff55dd87da3ea3060ddfef8b2bf
Woodaengtang <jhwoo200@gmail.com>
2025-04-02 22:23:31 +0900

    Update from lab

--- Optimization/conjugateGradientMethod.m
+++ Optimization/conjugateGradientMethod.m
@@ -1,4 +1,4 @@
-function [x_n, x_hstry] = conjugateGradientMethod(x, x_0, grad_f, alpha_k, beta_k, iter)
+function x_hstry = conjugateGradientMethod(x, x_0, grad_f, alpha_k, beta_k, iter)
     x_prev = x_0;
     x_hstry = [x_0, x];
     % x_n for problem solveing
@@ -9,9 +9,9 @@ function [x_n, x_hstry] = conjugateGradientMethod(x, x_0, grad_f, alpha_k, beta_
             g_prev = -grad_f(x_prev);
             d_prev = -g_prev;
         end
-        g = -grad_f(x);
+        g = grad_f(x);
         beta = beta_k(g, g_prev);
-        d = g + beta*d_prev;
+        d = -g + beta*d_prev;
         alpha = alpha_k(x, d);
         x = x_prev + alpha*d;
         g_prev = g;
@@ -19,6 +19,5 @@ function [x_n, x_hstry] = conjugateGradientMethod(x, x_0, grad_f, alpha_k, beta_
         x_prev = x;
         x_hstry = [x_hstry, x];
     end
-    x_n = x;
 end
 
