62f6c51f098c08a34fc2ca2ac4086666a7701837
Woodaengtang <jhwoo200@gmail.com>
2025-06-10 20:17:41 +0900

    Delete continuousOCP.m

--- Optimization/project/lib/continuousOCP.m
+++
@@ -1,9 +0,0 @@
-function dotx = continuousOPC(x_, beta)
-% State space eqn of OPC
-    dotx = NaN([length(x_), 1]);
-    dotx(1) = x_(3);
-    dotx(2) = x_(4);
-    dotx(3) = U*cos(beta);
-    dotx(4) = U*sin(beta);
-end
-
